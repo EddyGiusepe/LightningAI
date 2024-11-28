3e4a630a8ccf2fdc931052ae525fad3108cf2a50
EddyGiusepe <eddychirinos.unac@gmail.com>
2024-11-27 23:34:12 -0300

    Usando LSTM para Time-Series

---
+++ 1_PyTorch_Lightning_Hello_World/Time-Series_forecasting_with_PyTorch_Lightning.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Senior Data Scientist.: Dr. Eddy Giusepe Chirinos Isidro
+
+Time-Series forecasting with PyTorch Lightning
+==============================================
+Link de estudo ---> https://lightning.ai/lightning-ai/studios?view=public&section=featured&query=pytorch+lightning
+
+* For pip users:
+  --------------
+  pip install lightning
+* For conda users:
+  ----------------
+  conda install lightning -c conda-forge
+"""
+
